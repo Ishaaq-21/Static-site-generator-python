5b8b6f9be2e6039baf791d5662b319c2e6fcff68
isaac <ishak.hadj14@gmail.com>
2026-03-19 10:00:53 +0100

    write some tests for the parentnode class

--- src/tests/test_htmlnode.py
+++ src/tests/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.htmlnode import HTMLNode, LeafNode
+from src.htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -20,6 +20,21 @@ class TestLeafNode(unittest.TestCase):
     def test_eq(self): 
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+class TestParentNode(unittest.TestCase): 
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+       grandchild_node = LeafNode("b", "grandchild")
+       child_node2 = ParentNode("span", [grandchild_node])
+       parent_node2 = ParentNode("div", [child_node2])
+       self.assertEqual(
+           parent_node2.to_html(),
+           "<div><span><b>grandchild</b></span></div>",
+       )
 
 
 if __name__ == "__main__":
