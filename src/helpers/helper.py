222913b9ca1304aafc57db7466d61672fbd49788
isaac <ishak.hadj14@gmail.com>
2026-03-23 14:43:02 +0100

    implement functions for extracting markdown links and images

--- src/helpers/helper.py
+++ src/helpers/helper.py
@@ -1,4 +1,5 @@
 from src.textnode import TextType, TextNode
+import re
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     
@@ -26,3 +27,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
         
     return new_nodes
+
+
+
+
+def extract_markdown_images(text): 
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(pattern, text)
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    return re.findall(pattern, text)
