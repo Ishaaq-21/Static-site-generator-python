076f9b50dfcfbbfb0aaa360121c28f2765942776
isaac <ishak.hadj14@gmail.com>
2026-03-23 14:24:15 +0100

    write some tests for the split nodes function

--- src/tests/test_textnode.py
+++ src/tests/test_textnode.py
@@ -7,6 +7,24 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+    
+
+    def test_split_nodes(self): 
+        node = TextNode("This is a text node with *bold* and _italic_ text", TextType.TEXT)
+        nodes = [node]
+        from src.helpers.helper import split_nodes_delimiter
+        nodes = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+
+        expected_nodes = [
+            TextNode("This is a text node with ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" text", TextType.TEXT),
+        ]
+
+        self.assertEqual(nodes, expected_nodes)
 
 
 if __name__ == "__main__":
